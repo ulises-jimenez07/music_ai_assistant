b0d5639f35e7fa09f182656691643b8a26808f0f
Ulises Jimenez <ulises.jimenez07@gmail.com>
2025-04-16 22:22:01 -0600

    feat: commands to build and run containers

---
+++ docker_commands.sh
@@ -0,0 +1,9 @@
+docker build -t code-execution-service:latest -f code_execution_service/Dockerfile .
+docker run -d -p 8082:8082 --name code-execution-service code-execution-service:latest
+docker stop code-execution-service
+docker rm code-execution-service
+
+docker build -t code-generation-service:latest -f llm_service/Dockerfile .
+docker run -d -p 8081:8081 --name code-generation-service code-generation-service:latest
+docker stop code-generation-service
+docker rm code-generation-services
